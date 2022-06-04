d92df8d4a4207cee00c341e4c36434c427450cce
Alain Nguyen <46081985+Aloinn@users.noreply.github.com>
2022-06-04 16:04:18 -0400

    Added default exports

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,2 @@
 // FORMS
-export * from "@src/main/form/AutoInput.vue"; //"src/main/form/AutoInput.vue";
-export * from "@src/main/form/BooleanInput.vue";
-export * from "@src/main/form/DateInput.vue";
-export * from "@src/main/form/MediaInput.vue";
-export * from "@src/main/form/NumberInput.vue";
-export * from "@src/main/form/SelectDocInput.vue";
-export * from "@src/main/form/TextInput.vue";
-export * from "@src/main/form/TextSelectInput.vue";
-
-// RENDERERS
-export * from "@src/main/renderers/BooleanRenderer.vue";
-export * from "@src/main/renderers/CurrencyRenderer.vue";
-export * from "@src/main/renderers/DateRenderer.vue";
-export * from "@src/main/renderers/DocRenderer.vue";
-export * from "@src/main/renderers/ImageRenderer.vue";
-export * from "@src/main/renderers/LinkRenderer.vue";
+export * from "./main/components"; //"src/main/form/AutoInput.vue";
