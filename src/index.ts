7a6ef08d2721c20da3f60672bcbf7a92138c8bd6
Aloinn <46081985+Aloinn@users.noreply.github.com>
2022-06-13 19:19:31 -0400

    Testing release

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
 // FORMS
-export * from "./main/components"; //"src/main/form/AutoInput.vue";
+export { default as AutoInput } from "./main/form/AutoInput.vue";
+export { default as BooleanInput } from "./main/form/BooleanInput.vue";
+export { default as DateInput } from "./main/form/DateInput.vue";
+export { default as MediaInput } from "./main/form/MediaInput.vue";
+export { default as NumberInput } from "./main/form/NumberInput.vue";
+export { default as SelectDocInput } from "./main/form/SelectDocInput.vue";
+export { default as TextInput } from "./main/form/TextInput.vue";
+export { default as TextSelectInput } from "./main/form/TextSelectInput.vue";
+
+// RENDERERS
+export { default as BooleanRenderer } from "./main/renderers/BooleanRenderer.vue";
+export { default as CurrencyRenderer } from "./main/renderers/CurrencyRenderer.vue";
+export { default as DateRenderer } from "./main/renderers/DateRenderer.vue";
+export { default as DocRenderer } from "./main/renderers/DocRenderer.vue";
+export { default as ImageRenderer } from "./main/renderers/ImageRenderer.vue";
+export { default as LinkRenderer } from "./main/renderers/LinkRenderer.vue";
