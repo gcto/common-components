bc04d35bacf73b1053393d2e5d761be027744ca9
Aloinn <46081985+Aloinn@users.noreply.github.com>
2022-06-13 17:22:26 -0400

    Revert "Test upload"
    This reverts commit f6e51c56004c26d650699a8275e8c090c7d977e5.

---
+++ src/main/components.ts
@@ -0,0 +1,17 @@
+// FORMS
+export { default as AutoInput } from "./form/AutoInput.vue"; //"src/main/form/AutoInput.vue";
+export { default as BooleanInput } from "./form/BooleanInput.vue";
+export { default as DateInput } from "./form/DateInput.vue";
+export { default as MediaInput } from "./form/MediaInput.vue";
+export { default as NumberInput } from "./form/NumberInput.vue";
+export { default as SelectDocInput } from "./form/SelectDocInput.vue";
+export { default as TextInput } from "./form/TextInput.vue";
+export { default as TextSelectInput } from "./form/TextSelectInput.vue";
+
+// RENDERERS
+export { default as BooleanRenderer } from "./renderers/BooleanRenderer.vue";
+export { default as CurrencyRenderer } from "./renderers/CurrencyRenderer.vue";
+export { default as DateRenderer } from "./renderers/DateRenderer.vue";
+export { default as DocRenderer } from "./renderers/DocRenderer.vue";
+export { default as ImageRenderer } from "./renderers/ImageRenderer.vue";
+export { default as LinkRenderer } from "./renderers/LinkRenderer.vue";
